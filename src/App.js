6d4f25db21c0a42916128d84f7037696325060c4
coord.e <me@coord-e.com>
2018-07-31 21:11:38 +0900

    Add: Add elements

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="control">
+        <div className="control-up">
+          ↑
+        </div>
+        <div className="control-right">
+          →
+        </div>
+        <div className="control-left">
+          ←
+        </div>
+        <div className="control-down">
+          ↓
+        </div>
       </div>
     );
   }
